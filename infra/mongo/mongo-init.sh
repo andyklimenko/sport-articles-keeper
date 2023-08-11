46cc8fe9d162156eb73a15c3e6867d9919c9a545
Andrii Klymenko <andrii.klymenko@underdefense.com>
2023-08-11 18:32:25 +0200

    creating indexes

--- infra/mongo/mongo-init.sh
+++ infra/mongo/mongo-init.sh
@@ -4,5 +4,7 @@ mongo <<EOF
 
 use ${MONGO_INITDB_DATABASE}
 db.createCollection('newsletterNews')
+db.newsletterNews.createIndex( { newsArticleId: 1} )
+db.newsletterNews.createIndex( { id: 1} )
 
 EOF
